38d49d3cc67dd11e62084954170d064e0568be74
ronik-v <andronnikovvv@gmail.com>
2023-04-06 08:44:04 +0300

    second bot update

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import Dispatcher, FSMContext
 from aiogram.types import ReplyKeyboardMarkup
 from aiogram.dispatcher.filters import Text
-import aiogram.utils.markdown as md
 from aiogram.utils import executor
 from aiogram import Bot, types
+import aiogram.utils.markdown as md
 
 from PortfolioFilters import MakeBetaPositivePortfolio, IncomeTickerFilter, VolatilityTickerFilter
-from ShowTickerDynamics import TickerDynamics, JapaneseCandlesDynamics
+from PriceСharts import TickerDynamics, JapaneseCandlesDynamics
 from PortfolioModels import MarkovModel
 from TickerDataParser import DataParser
 
@@ -18,7 +18,7 @@ from config import bot_token
 import logging
 
 today = datetime.now()
-end_date = today - timedelta(weeks=12)  # 3 months earlier
+end_date = today - timedelta(weeks=10)  # ~3 months earlier
 date_start, date_end = end_date.strftime('%Y-%m-%d'), today.strftime('%Y-%m-%d')
 bot = Bot(token=bot_token)
 dp = Dispatcher(bot, storage=MemoryStorage())
@@ -33,21 +33,13 @@ except OSError:
     print("\033[31m {}".format("Failed to change directory to Graphs."))
     exit(1)
 
-keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-systems_buttons = ['Помощь', 'Описание бота']
-portfolios_buttons = ['Создать обычный портфель', 'Создать портфель с бета значением',
-                      'Создать портфель с максимальным доходом', 'Создать портфель с минимальной волатильностью', ]
-graphs_buttons = ['Создать график скользящих средних', 'Создать график японских свечей']
-keyboard.add(systems_buttons[0])
-keyboard.add(systems_buttons[1]).insert(portfolios_buttons[0]).insert(portfolios_buttons[1]).insert(
-    portfolios_buttons[2]).insert(portfolios_buttons[3])
-keyboard.add(*graphs_buttons)
 P_COMMAND = []
 P_COST = []
 G_COMMAND = []
 
 
-async def send(message: types.Message, structure: list):
+#   Data sending functions
+async def send_text(message: types.Message, structure: list):
     for mes in structure:
         await bot.send_message(message.chat.id, md.text(mes))
 
@@ -59,6 +51,15 @@ async def send_photo(message: types.Message, png_file):
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    systems_buttons = ['Помощь', 'Описание бота']
+    portfolios_buttons = ['Создать обычный портфель', 'Создать портфель с бета значением',
+                          'Создать портфель с максимальным доходом', 'Создать портфель с минимальной волатильностью', ]
+    graphs_buttons = ['Создать график скользящих средних', 'Создать график японских свечей']
+    keyboard.add(systems_buttons[0])
+    keyboard.add(systems_buttons[1]).insert(portfolios_buttons[0]).insert(portfolios_buttons[1]).insert(
+        portfolios_buttons[2]).insert(portfolios_buttons[3])
+    keyboard.add(*graphs_buttons)
     await bot.send_message(
         reply_markup=keyboard,
         chat_id=message.from_user.id,
@@ -85,10 +86,9 @@ async def description_command(message: types.Message):
     )
 
 
-@dp.message_handler(lambda message: message.text == 'Создать обычный портфель'
-                    or message.text == 'Создать портфель с бета значением'
-                    or message.text == 'Создать портфель с максимальным доходом'
-                    or message.text == 'Создать портфель с минимальной волатильностью')
+@dp.message_handler(lambda message: message.text in [
+    'Создать обычный портфель', 'Создать портфель с бета значением',
+    'Создать портфель с максимальным доходом', 'Создать портфель с минимальной волатильностью'])
 async def create_portfolio_command(message: types.Message):
     P_COMMAND.append(message.text)
     await TickersListForms.portfolio_cost.set()
@@ -130,23 +130,25 @@ async def portfolio_result(message: types.Message, state: FSMContext):
                 tickers[position] = ticker[1:]
         if command == 'Создать обычный портфель':
             markov_p = MarkovModel(DataParser(tickers, date_start, date_end).parse_tickers(), cost).result()
-            await send(message, markov_p)
+            await send_text(message, markov_p)
         if command == 'Создать портфель с бета значением':
             beta_tickers = MakeBetaPositivePortfolio(tickers, date_start, date_end).BetaPositivePortfolio(tickers,
                                                                                                           date_start,
                                                                                                           date_end)
             markov_p_beta = MarkovModel(DataParser(beta_tickers, date_start, date_end).parse_tickers(), cost).result()
-            await send(message, markov_p_beta)
+            await send_text(message, markov_p_beta)
         if command == 'Создать портфель с максимальным доходом':
             income_tickers = IncomeTickerFilter(tickers, date_start, date_end).income_filter()
             markov_p_income = MarkovModel(DataParser(income_tickers, date_start, date_end).parse_tickers(),
                                           cost).result()
-            await send(message, markov_p_income)
+            await send_text(message, markov_p_income)
         if command == 'Создать портфель с минимальной волатильностью':
             volatility_tickers = VolatilityTickerFilter(tickers, date_start, date_end).filter()
             markov_p_volatility = MarkovModel(DataParser(volatility_tickers, date_start, date_end).parse_tickers(),
                                               cost).result()
-            await send(message, markov_p_volatility)
+            await send_text(message, markov_p_volatility)
+        del P_COMMAND[:]
+        del P_COST[:]
     except:
         logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
         logging.info(f'portfolio {message.from_user.id}-{message.from_user.first_name}')
@@ -155,8 +157,8 @@ async def portfolio_result(message: types.Message, state: FSMContext):
         await state.finish()
 
 
-@dp.message_handler(lambda message: message.text == 'Создать график скользящих средних'
-                                    or message.text == 'Создать график японских свечей')
+@dp.message_handler(lambda message: message.text in ['Создать график скользящих средних',
+                                                     'Создать график японских свечей'])
 async def create_graph_command(message: types.Message):
     G_COMMAND.append(message.text)
     await TickerForm.ticker.set()
@@ -174,6 +176,7 @@ async def graph_result(message: types.Message, state: FSMContext):
         if command == 'Создать график японских свечей':
             file = JapaneseCandlesDynamics(ticker, date_start, date_end, message.from_id).candles_graph()
             await send_photo(message, file)
+        del G_COMMAND[:]
     except:
         logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
         logging.info(f'graph {message.from_user.id}-{message.from_user.first_name}')
@@ -184,6 +187,6 @@ async def graph_result(message: types.Message, state: FSMContext):
 
 if __name__ == '__main__':
     try:
-        executor.start_polling(dp)
+        executor.start_polling(dp, skip_updates=True)
     except OSError as Error:
         print(Error)
