d2a9c6a165df11bb7ec2d584f8018afbb97ea555
ronik-v <andronnikovvv@gmail.com>
2023-08-18 12:47:31 +0300

    data parser update

--- TickerDataParser.py
+++ TickerDataParser.py
@@ -9,6 +9,25 @@ class DataParser:
         self.date_end = date_end
         self.DF_tickers = DataFrame(columns=tickers)
 
+    @classmethod
+    def get_base_df(cls, ticker: str, date_start: str, date_end: str) -> DataFrame:
+        return pdr.data.DataReader(ticker, 'moex', date_start, date_end)
+
+    @classmethod
+    def filter_parser(cls, tickers_list: list[str], date_start: str, date_end: str) -> dict[str, DataFrame]:
+        tickers_prices = dict()
+        for ticker in tickers_list:
+            new_df = DataFrame(columns=['SMA(5)', 'STD', 'DIFF'])
+            df_ticker = cls.get_base_df(ticker, date_start, date_end)
+            df_ticker['SMA(5)'] = df_ticker['CLOSE'].rolling(5).mean()
+            df_ticker['STD'] = df_ticker['CLOSE'].rolling(5).std()
+            df_ticker['DIFF'] = df_ticker['CLOSE'] - df_ticker['OPEN']
+            new_df['SMA(5)'] = df_ticker['SMA(5)']
+            new_df['STD'] = df_ticker['STD']
+            new_df['DIFF'] = df_ticker['DIFF']
+            tickers_prices[ticker] = new_df
+        return tickers_prices
+
     def parse_ticker_for_dynamics(self, ticker) -> DataFrame:
         df = pdr.data.DataReader(ticker, 'moex', self.date_start, self.date_end)['CLOSE']
         df['SMA5'] = df['CLOSE'].rolling(5).mean()
