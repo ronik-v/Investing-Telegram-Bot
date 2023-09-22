f0cd01c664d0720a5f163db3f385fd5bce720c59
ronik-v <andronnikovvv@gmail.com>
2023-09-22 23:40:37 +0300

    setup

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
 
+# load libs and readme...
+with open('requirements.txt') as file:
+	requires = file.readlines()
+
+with open('README.md') as file:
+	readme = file.readlines()
+
+
 setup(
 	author='ronik-v',
 	name='Investing Bot',
 	license='Apache-2.0 license',
 	packages=find_packages(include=['src']),
-	install_requires=[
-		'aiogram', 'pandas', 'pandas-datareader', 'numpy', 'matplotlib'
-	]
+	python_requires='>=3.11',
+	url='https://github.com/ronik-v/Investing-Telegram-Bot'
 )
